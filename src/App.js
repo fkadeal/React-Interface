1577a935399d39262638b28787d1ccfefa42ca2e
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-04-22 03:23:58 -0800

    add Appointment component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { BiArchive } from "react-icons/bi"
-import Search from "./components/Search"
+import { BiArchive } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-     <h1 className="text-5xl">
+     <h1 className="text-5xl mb-5">
+
        <BiArchive className="inline-block text-red-400"/>your first Appointment</h1>
+       <AddAppointment />
        <Search />
     </div>
   );
